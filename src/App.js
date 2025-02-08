65b456998ef2e18a3311abadec25b72ddefc7574
felort1512 <felort15@gmail.com>
2025-02-07 21:23:45 -0600

    Cereation of ToDo`s futions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from './platzi.webp';
 import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 
 function App() {
   return (
     <div className="App">
+        <TodoCounter />
+      <TodoSearch />
+
+       <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+       </TodoList>
+
+       <CreateTodoButton />
+        
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
